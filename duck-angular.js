7a4b9a715784076ca928a5711c83ebad68c8cd6f
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2013-07-15 20:19:18 +0530

    Adding support for deferred interactions to allow hooks into promise-based callbacks - <Mojo>

--- duck-angular.js
+++ duck-angular.js
@@ -15,8 +15,8 @@ var Container = function Container() {
 		return self.rootScope.$new();
 	};
 
-	this.controller = function(controllerName, scope) {
-		return self.controllerProvider(controllerName, {$scope: scope});
+	this.controller = function(controllerName, mocks) {
+		return self.controllerProvider(controllerName, mocks);
 	};
 
 	this.view = function(viewUrl, controller, scope) {
@@ -33,19 +33,51 @@ var Container = function Container() {
 		return deferred.promise;
 	};
 
-	this.mvc = function(controllerName, viewUrl) {
+	this.mvc = function(controllerName, viewUrl, mocks) {
+    mocks = mocks ? mocks : {};
 		var scope = self.newScope();
-		var controller = self.controller(controllerName, scope);
+    mocks.$scope = scope;
+
+		var controller = self.controller(controllerName, mocks);
 		return this.view(viewUrl, controller, scope).then(function(compiledTemplate) {
-			console.log("Controller is " + controller);
-			console.log("View is " + compiledTemplate);
-			console.log("Scope is " + scope);
 			return { controller: controller, view: compiledTemplate, scope: scope };
 		});
 	};
 };
 
+var DuckInteraction = function DuckInteraction(interaction) {
+  this.do = interaction;
+
+  this.waitFor = function(o, fn) {
+    this.waitingFor = function(then) {
+      var deferred = Q.defer();
+      var originalFn = o[fn];
+      o[fn] = function() {
+        return originalFn.apply(o, arguments).then(function(result) {
+          try {
+            then();
+            deferred.resolve();
+          } catch(e) {
+            deferred.reject(e);
+          } finally {
+            return result;
+          }
+        });
+      };
+      return deferred.promise;
+    };
+    return this;
+  };
+
+  this.after =  function(then) {
+    var interactionPromise = this.waitingFor(then);
+    this.do();
+    return interactionPromise;
+  }
+};
+
 var DuckDOM = function DuckDOM(view, scope) {
+  var self = this;
 	this.interactWith = function(selector, value) {
 		var elements = angular.element(selector, view);
 
@@ -59,15 +91,42 @@ var DuckDOM = function DuckDOM(view, scope) {
 			else if (element.nodeName === "INPUT" && element.type === "checkbox") {
 				elements.click().trigger("click");
 			}
-			else if (element.nodeName === "SELECT") {
-			angular.element(elements[0].options[value]).attr("selected", true);
-			elements.trigger("change");
-			}
+      else if (element.nodeName === "SELECT") {
+        angular.element(elements[0].options[value]).attr("selected", true);
+        elements.trigger("change");
+      }
 		});
 		scope.$apply();
+    return self;
  	};
+
+  this.interactAndWait = function(selector, value) {
+    return new DuckInteraction(function() {
+      self.interactWith(selector, value);
+      return this;
+    });
+  };
 
 	this.element = function(selector) {
 		return angular.element(selector, view);
 	};
+
+  this.assertAfter = function(o, fn, assertBlock) {
+    var deferred = Q.defer();
+    var originalFn = o[fn];
+    o[fn] = function() {
+        return originalFn.apply(o, arguments).then(function(result) {
+          try {
+            assertBlock();
+            deferred.resolve();
+          } catch(e) {
+            deferred.reject(e);
+          } finally {
+            return result;
+          }
+        });
+    };
+
+    return deferred.promise;
+  }
 };
