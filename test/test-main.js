62fbd075a6ad10e9bd246fb82eea9b69c6f676ad
Michael Lawrie <michael@michaellawrie.com>
2014-07-03 23:36:53 -0400

    Add test framework <Michael>

---
+++ test/test-main.js
@@ -0,0 +1,34 @@
+var tests = [];
+for (var file in window.__karma__.files) {
+  if (window.__karma__.files.hasOwnProperty(file)) {
+    if (/spec\.js$/.test(file)) {
+      tests.push(file);
+    }
+  }
+}
+
+requirejs.config({
+  // Karma serves files from '/base'
+  baseUrl: '/base',
+
+  paths: {
+    'angular': 'node_modules/angular/lib/angular',
+    'underscore': 'node_modules/underscore/underscore',
+    'q': 'node_modules/q/q',
+    'jquery': 'node_modules/jquery/dist/jquery',
+    'text': 'node_modules/text/text'
+  },
+
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'angular': {
+      exports: 'angular'
+    }
+  },
+
+
+  deps: tests,
+  callback: window.__karma__.start
+});
